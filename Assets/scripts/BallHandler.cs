58d5225ec3a255326d4c4f055f405f2bbc9927bd
Mahmoud0090 <mmh.00090@gmail.com>
2022-02-24 00:13:09 +0300

    Launching the Ball

--- Assets/scripts/BallHandler.cs
+++ Assets/scripts/BallHandler.cs
@@ -5,7 +5,14 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    
+    [SerializeField] private Rigidbody2D currentBallRigidbody2D;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint2D;
+    [SerializeField] float detachDelay;
+
+
     private Camera mainCamera;
+    private bool isDragging;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +22,43 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (currentBallRigidbody2D == null) { return; }
+
         if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+
+            isDragging = false;
+
             return;
         }
+
+        isDragging = true;
+
+        currentBallRigidbody2D.isKinematic = true;
 
         Vector2 touchPosition =  Touchscreen.current.primaryTouch.position.ReadValue();
 
         Vector2 worldPostion = mainCamera.ScreenToWorldPoint(touchPosition);
 
-        Debug.Log(worldPostion);
+        currentBallRigidbody2D.position = worldPostion;
+
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidbody2D.isKinematic = false;
+        currentBallRigidbody2D = null;
+
+        Invoke(nameof(DetachTheBall), detachDelay);
+    }
+
+    private void DetachTheBall()
+    {
+        currentBallSpringJoint2D.enabled = false;
+        currentBallSpringJoint2D = null;
     }
 }
